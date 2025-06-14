6a6c7f1d9f759d743e9a473bde37fb2f5fae18ec
nada <1bharold@gmail.com>
2025-06-13 21:34:00 -0500

    mi primer commit

---
+++ src/main/java/com/proyecto/jpa/ProyectoFinalLp2Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalLp2Application.class, args);
+	}
+
+}
